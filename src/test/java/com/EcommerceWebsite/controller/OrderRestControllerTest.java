80bb31100c82f8492f52108ec4af24f3e79dd83e
Shree Kumar <shree181203@gmail.com>
2025-11-18 23:06:29 +0530

    update files

--- src/test/java/com/EcommerceWebsite/controller/OrderRestControllerTest.java
+++ src/test/java/com/EcommerceWebsite/controller/OrderRestControllerTest.java
@@ -5,7 +5,6 @@ import com.EcommerceWebsite.model.User;
 import com.EcommerceWebsite.service.OrderService;
 import com.EcommerceWebsite.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
